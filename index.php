9ce76bac89cef9cd8393c92351f4451d03c1a412
deer2333 <80447412+deer2333@users.noreply.github.com>
2021-04-15 18:02:55 +0800

    index.php

--- index.php
+++ index.php
@@ -5,41 +5,40 @@
 </head>
 <body>
 <?php
+
 $curl = curl_init();
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 
-if (isset($_GET['species'])) {
-	$species = $_GET['species'];
 
-}
-else{
-	$species ="orca";
-}
+
 $query = array(
-
+   
 	"apikey"=>"k_lnrd0r36",
-	"limit"=>'200'
+	
 	 );
 
 
 
-$url = "https://imdb-api.com/zh_CN/API/Title/k_12345678/tt1375666"."?".http_build_query($query);
+$url = "https://imdb-api.com/zh_CN/API/MostPopularMovies/k_lnrd0r36"."?".http_build_query($query);
 
 curl_setopt($curl, CURLOPT_URL, $url);
 
-$result = json_
+$result = json_decode(curl_exec($curl));
 
-
-decode(curl_exec($curl));
-
-
-foreach ($result as $sightings ) {
-	print($sightings->location);
-		print"</br> ";
-
+foreach ($result->items as $sightings ) {
+	
+	print $sightings->image;
+	print "</br>";
+	print $sightings->title;
+	print "</br>";
+	print $sightings->year;
+	print "</br>";
+	print $sightings->imDbRating;
+	print "</br>";
 };
+
 
 
 ?>
 </body>
-</html>>
+</html>
