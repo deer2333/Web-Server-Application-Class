b85f24ee2dfb0167695edea66a46f537e4f3c05a
deer2333 <80447412+deer2333@users.noreply.github.com>
2021-04-16 17:36:38 +0800

    css.php

--- movie_api.php
+++ movie_api.php
@@ -2,8 +2,48 @@
 <html>
 <head>
 	<title> Movie </title>
+	<?php require 'css.php'?>
 </head>
 <body>
+<nav class="navbar navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="Image/camera-reels.svg" class="d-inline-block align-text-top" style="width: 50px;height: 45px;">
+     <div class="title1"> Movie</div>
+    </a>
+  </div>
+</nav>
+
+
+<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="Image/p2638199066.webp.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="Image/p2580430938.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="Image/p2638199066.webp.jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+
+
 	<div class= "head">
 <?php
 
@@ -49,7 +89,7 @@ foreach ($result->items as $sightings ) {
 .head{
    text-align:center;
    background-color: #E6E6FA;
-   height: 800px;
+ 
 }
 <style>
 </html>
